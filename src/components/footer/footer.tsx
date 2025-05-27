f0e8ff9984451b884f2d86bb5f779065a89e6de1
Mario Silva <mbm3d2000@yahoo.com.br>
2025-05-27 05:46:28 -0300

    Correção de Cores tailwind.config.ts. globalcss e programas layut header footer button

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,14 +3,14 @@ import { Logo } from "../logo"
 
 export const Footer = () => {
     return (
-        <footer className="border-t border-white/10 bg-background/95">
+        <footer className="bg-gray-500">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between md:flex-row gap-8 py-8">
                     <Logo />
-                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
-                        <Link href="/termo-de-uso">Termo de uso</Link>
-                        <Link href="/politica-de-privacidade">Política de privacidade</Link>
-                        <Link href="/feedback">Feedback</Link>
+                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-blue-100">
+                        <Link href="/termo-de-uso" className="houver:text-blue-200">Termo de uso</Link>
+                        <Link href="/politica-de-privacidade" className="houver:text-blue-200">Política de privacidade</Link>
+                        <Link href="/feedback" className="houver:text-blue-200">Feedback</Link>
                     </nav>
                 </div>
             </div>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,16 @@
+import { Inter } from "next/font/google";
 import { Footer } from "../footer";
 import { Header } from "../header";
 
 type LayoutProps = {
     children: React.ReactNode;
 }
+
+const inter = Inter({ subsets: ['latin']})
+
 export const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className="relative flex min-h-screen flex-col bg-gray-700 dark">
+        <div className={'${inter.className} relative flex min-h-screen flex-col bg-gray-700'}>
             <Header />
             <main className="flex-1 flex flex-col mb-12">
                 {children}
