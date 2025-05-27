93ae0760b0aa0a4fcb3701516a809ba8444c737d
Mario Silva <mbm3d2000@yahoo.com.br>
2025-05-27 19:18:00 -0300

    Criado segunda seção feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
     <>
       <article className="flex flex-col">
-        <HeroSection/>
+        <HeroSection />
+        <FeatureSection />
       </article>
     </>
   );
