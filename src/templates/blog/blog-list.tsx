7510c17bd49e061c9e6733395ca41fc6db75ac35
Mario Silva <mbm3d2000@yahoo.com.br>
2025-06-02 07:18:13 -0300

    Listagem dinâmica dos posts e configuração do contentlayer

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,8 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
+import { allPosts } from "contentlayer/generated";
 
 export function BlogList() {
     const router = useRouter();
@@ -7,9 +10,11 @@ export function BlogList() {
 
     const pageTitle = query ? `Resultado de busca para "${query}"` : 'Dicas e estratégias para impulsionar seu negócio'
 
+    const posts = allPosts; // vem do conttantelayer
+
     return (
         <div className="flex flex-col py-24 flex-grow h-full">
-            <header className="">
+            <header className="pb-14"> {/* pading botton 56px */}
                 <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
                     <div className="flex flex-col gap-4 md:px-0">
                         {/* TAG */}
@@ -26,6 +31,52 @@ export function BlogList() {
                     <Search />
                 </div>
             </header>
+            {/* Listagem de posts */}
+            {/* Primeira Modificação só para deixar funcionando 
+            <PostCard 
+                title="Tranformando se negócio em uma loja virtual"    
+                description="Se você está buscanco uma maneira..."
+                date="20/12/2024"
+                slug="/Tranformando"
+                image="/assets/primeiro-post.png"
+                author={{
+                    avatar: '/customer-02.png',
+                    name: 'Aspen Dokidis'
+                }}
+            />
+            */}
+            {/* Segunda Modificação Listagem de posts envolvendo com grid 
+            <PostGridCard>
+                <PostCard 
+                    title="Tranformando se negócio em uma loja virtual"    
+                    description="Se você está buscanco uma maneira..."
+                    date="20/12/2024"
+                    slug="/Tranformando"
+                    image="/assets/primeiro-post.png"
+                    author={{
+                        avatar: '/customer-02.png',
+                        name: 'Aspen Dokidis'
+                    }}
+                />
+           </PostGridCard>
+           */}
+           {/* Listagem de posts */}
+            <PostGridCard>
+                {posts.map((post) => (
+                <PostCard 
+                    key={post._id}
+                    title={post.title}
+                    description={post.description}
+                    date={new Date(post.date).toLocaleDateString("pt-BR")}
+                    slug={post.slug}
+                    image={post.image}
+                    author={{
+                        avatar: post.author?.avatar,
+                        name: post.author?.name,
+                    }}
+                />
+                ))}
+            </PostGridCard>
         </div>
     )
 }
