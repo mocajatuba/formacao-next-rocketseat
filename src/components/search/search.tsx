3accd365733d2188655c22102b89d949f99a8115
Mario Silva <mbm3d2000@yahoo.com.br>
2025-06-12 12:53:21 -0300

    Criado not-found.tsx exibição de erro 404 e retorn com get na busca

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation"
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export const Search = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const router = useRouter();
     const searchParams = useSearchParams();
     const query = searchParams?.get('q') ?? '';
+    const hasQuery = !!searchParams?.has('q');
 
     const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault();
@@ -29,11 +31,19 @@ export const Search = () => {
             scroll: false
         })
     }
+
+    useEffect(() => {
+        if(hasQuery) {
+            inputRef.current?.focus()
+        } 
+    }, [hasQuery])
+
     return (
         <form onSubmit={handleSearch} className="relative group w-full md:w-60">
             <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300', query ? 'text-blue-300': '')}/>
 
             <input 
+                ref={inputRef}
                 type="text"
                 placeholder="Buscar"
                 onChange={handleQueryChange}
